57f672685bc3c2dc2d221cea34c6d88e87396b5a
libjared <libjared@users.noreply.github.com>
2016-10-26 03:27:26 -0400

    play mod file

--- source/music.c
+++ source/music.c
@@ -5,14 +5,28 @@ Result musicinit() {
 	Result res = csndInit();
 	printf("csndInit = %li\n", res);
 
-	//generate buffer
+	//allocate buffer
 	u32* soundBuf = linearAlloc(sizeof(u32) * SOUND_BUF_SIZE);
-	for (int i = 0; i < SOUND_BUF_SIZE; i++) {
-		soundBuf[i] = rand();
-	}
+
+	//init xmp for playing .mod file
+	musicCtx = xmp_create_context();
+
+	//read module file
+	Handle fsHandle;
+	u32 fsSize;
+	fsopen(&fsHandle, &fsSize, "/3ds/ctrmagic/fury.lagon.mod");
+	moduleBuffer = linearAlloc(sizeof(char) * fsSize);
+	fsread(fsHandle, fsSize, moduleBuffer);
+
+	int xmpres = xmp_load_module_from_memory(musicCtx, moduleBuffer, fsSize);
+	printf("xmp load module = %i\n", xmpres);
+	xmpres = xmp_start_player(musicCtx, SOUND_SAMPLE_RATE, XMP_FORMAT_MONO);
+	printf("xmp start player = %i\n", xmpres);
+	xmp_play_buffer(musicCtx, soundBuf, SOUND_BUF_SIZE, 0);
+	printf("xmp play buffer = %i\n", xmpres);
 
 	//play buffer
-	res = csndPlaySound(0x8, SOUND_REPEAT | SOUND_FORMAT_16BIT, 8000, 1.0, 0.0, (u32*)soundBuf, (u32*)soundBuf, SOUND_BUF_SIZE);
+	res = csndPlaySound(0x8, SOUND_ONE_SHOT | SOUND_FORMAT_16BIT, SOUND_SAMPLE_RATE, 1.0, 0.0, (u32*)soundBuf, NULL, SOUND_BUF_SIZE);
 	printf("csndPlaySound = %li\n", res);
 
 	return res;
@@ -20,5 +34,6 @@ Result musicinit() {
 
 void musicFree() {
 	linearFree(soundBuf);
+	linearFree(moduleBuffer);
     csndExit();
 }
--- source/music.h
+++ source/music.h
@@ -7,9 +7,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <libxmp-lite/xmp.h>
+#include "myfs.h"
 
 u32* soundBuf;
-#define SOUND_BUF_SIZE 1000
+char* moduleBuffer;
+xmp_context musicCtx;
+#define SOUND_SAMPLE_RATE 44100
+#define SOUND_BUF_SIZE (SOUND_SAMPLE_RATE*1*2) //1 second * 2 for stereo
 
 Result musicinit();
 
